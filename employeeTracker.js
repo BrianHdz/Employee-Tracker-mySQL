8cb955434cdcb8fa51ff6f7299ef88d0142d27e3
BrianHdz <popcorn46290@sbcglobal.net>
2020-02-12 22:18:55 -0800

    The first set of questions is set. Need to create functions for each one of the questions.

--- employeeTracker.js
+++ employeeTracker.js
@@ -2,20 +2,69 @@ var inquirer = require("inquirer");
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "PASSWORD",
-  database: "employee_tracker_DB"
+    // Your password
+    password: "PASSWORD",
+    database: "employee_tracker_DB"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  runSearch();
+connection.connect(function (err) {
+    if (err) throw err;
+    runSearch();
 });
+
+function runSearch() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "rawlist",
+            message: "What would you like to do?",
+            choices: [
+                "Add Employee",
+                "Add Department",
+                "Add Roles",
+                "View All Employees",
+                "View All Departments",
+                "View All Roles",
+                "Update Employee Roles"
+            ]
+        })
+        .then(function (answer) {
+            switch (answer.action) {
+                case "Add Employee":
+                    addEmployee();
+                    break;
+
+                case "Add Department":
+                    addDepartment();
+                    break;
+
+                case "Add Roles":
+                    addRoles();
+                    break;
+
+                case "View All Employees":
+                    viewEmployees();
+                    break;
+
+                case "View All Departments":
+                    viewDepartments();
+                    break;
+
+                case "View All Roles":
+                    viewRoles();
+                    break;
+
+                case "Update Employee Roles":
+                    updateRoles();
+                    break;
+            }
+        });
+}
